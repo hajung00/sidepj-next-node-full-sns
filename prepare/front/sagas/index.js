54dad8611d68127b451fe2af2a31d99c27b87ce9
hajung00 <gkwjd719@naver.com>
2023-01-31 15:07:36 +0900

    Fix: backUrl

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -6,7 +6,7 @@ import userSaga from './user';
 import postSaga from './post';
 import { backUrl } from '../config/config';
 import axios from 'axios';
-axios.defaults.baseURL = { backUrl };
+axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
