0ffaac8c5c7841ba573651d51c0cdc00035bf7f8
hajung00 <gkwjd719@naver.com>
2023-02-01 23:26:26 +0900

    fix: allpost

--- prepare/front/pages/allpost.js
+++ prepare/front/pages/allpost.js
@@ -12,97 +12,98 @@
 // import { LOAD_MY_INFO_REQUEST } from '../reducers/user';
 // import wrapper from '../store/configureStore';
 
-// const AllPosts = () => {
-//   const dispatch = useDispatch();
-//   const { me } = useSelector((state) => state.user);
-//   const {
-//     mainPosts,
-//     hasMorePosts,
-//     loadPostLoading,
-//     retweetError,
-//     accusePostError,
-//     accuseMessage,
-//   } = useSelector((state) => state.post);
-//   useEffect(() => {
-//     if (me) {
-//       const lastId = 0;
-//       dispatch({
-//         type: LOAD_POSTS_REQUEST,
-//       });
-//     }
-//   }, []);
+const AllPosts = () => {
+  <></>;
+  //   const dispatch = useDispatch();
+  //   const { me } = useSelector((state) => state.user);
+  //   const {
+  //     mainPosts,
+  //     hasMorePosts,
+  //     loadPostLoading,
+  //     retweetError,
+  //     accusePostError,
+  //     accuseMessage,
+  //   } = useSelector((state) => state.post);
+  //   useEffect(() => {
+  //     if (me) {
+  //       const lastId = 0;
+  //       dispatch({
+  //         type: LOAD_POSTS_REQUEST,
+  //       });
+  //     }
+  //   }, []);
 
-//   useEffect(() => {
-//     if (retweetError) {
-//       alert(retweetError);
-//     }
-//   }, [retweetError]);
+  //   useEffect(() => {
+  //     if (retweetError) {
+  //       alert(retweetError);
+  //     }
+  //   }, [retweetError]);
 
-//   useEffect(() => {
-//     if (accusePostError) {
-//       alert(accusePostError);
-//     }
-//   }, [accusePostError]);
+  //   useEffect(() => {
+  //     if (accusePostError) {
+  //       alert(accusePostError);
+  //     }
+  //   }, [accusePostError]);
 
-//   useEffect(() => {
-//     if (accuseMessage) {
-//       alert(accuseMessage);
-//     }
-//   }, [accuseMessage]);
+  //   useEffect(() => {
+  //     if (accuseMessage) {
+  //       alert(accuseMessage);
+  //     }
+  //   }, [accuseMessage]);
 
-//   // scroll
-//   useEffect(() => {
-//     const onScroll = () => {
-//       if (
-//         window.scrollY + document.documentElement.clientHeight >
-//         document.documentElement.scrollHeight - 300
-//       ) {
-//         if (hasMorePosts && !loadPostLoading) {
-//           const lastId = mainPosts[mainPosts.length - 1]?.id;
-//           dispatch({
-//             type: LOAD_POSTS_REQUEST,
-//             lastId,
-//           });
-//         }
-//       }
-//     };
-//     window.addEventListener('scroll', onScroll);
-//     return () => {
-//       window.removeEventListener('scroll', onScroll);
-//     };
-//   }, [hasMorePosts, loadPostLoading, mainPosts]);
+  //   // scroll
+  //   useEffect(() => {
+  //     const onScroll = () => {
+  //       if (
+  //         window.scrollY + document.documentElement.clientHeight >
+  //         document.documentElement.scrollHeight - 300
+  //       ) {
+  //         if (hasMorePosts && !loadPostLoading) {
+  //           const lastId = mainPosts[mainPosts.length - 1]?.id;
+  //           dispatch({
+  //             type: LOAD_POSTS_REQUEST,
+  //             lastId,
+  //           });
+  //         }
+  //       }
+  //     };
+  //     window.addEventListener('scroll', onScroll);
+  //     return () => {
+  //       window.removeEventListener('scroll', onScroll);
+  //     };
+  //   }, [hasMorePosts, loadPostLoading, mainPosts]);
 
-//   return (
-//     <>
-//       <AppLayout>
-//         {/* 로그인해야 게시물 작성 가능 */}
-//         {me && <PostForm />}
-//         {/* 작성글이 있으면 보여줌 */}
-//         {mainPosts.map((post) => (
-//           <PostCard key={post.id} post={post} />
-//         ))}
-//       </AppLayout>
-//     </>
-//   );
-// };
+  //   return (
+  //     <>
+  //       <AppLayout>
+  //         {/* 로그인해야 게시물 작성 가능 */}
+  //         {me && <PostForm />}
+  //         {/* 작성글이 있으면 보여줌 */}
+  //         {mainPosts.map((post) => (
+  //           <PostCard key={post.id} post={post} />
+  //         ))}
+  //       </AppLayout>
+  //     </>
+  //   );
+  // };
 
-// export const getServerSideProps = wrapper.getServerSideProps(
-//   async (context) => {
-//     const cookie = context.req ? context.req.headers.cookie : '';
-//     axios.defaults.headers.Cookie = '';
-//     if (context.req && cookie) {
-//       axios.defaults.headers.Cookie = cookie;
-//     }
+  // export const getServerSideProps = wrapper.getServerSideProps(
+  //   async (context) => {
+  //     const cookie = context.req ? context.req.headers.cookie : '';
+  //     axios.defaults.headers.Cookie = '';
+  //     if (context.req && cookie) {
+  //       axios.defaults.headers.Cookie = cookie;
+  //     }
 
-//     context.store.dispatch({
-//       type: LOAD_MY_INFO_REQUEST,
-//     });
-//     context.store.dispatch({
-//       type: LOAD_POSTS_REQUEST,
-//     });
-//     context.store.dispatch(END);
-//     await context.store.sagaTask.toPromise();
-//   }
-// );
+  //     context.store.dispatch({
+  //       type: LOAD_MY_INFO_REQUEST,
+  //     });
+  //     context.store.dispatch({
+  //       type: LOAD_POSTS_REQUEST,
+  //     });
+  //     context.store.dispatch(END);
+  //     await context.store.sagaTask.toPromise();
+};
+//);
 
-// export default AllPosts;
+export default AllPosts;
