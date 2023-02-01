9481dee2c04c11893cbcb4c898c07bc45e65bc9f
hajung00 <gkwjd719@naver.com>
2023-02-01 19:18:00 +0900

    fix

--- prepare/front/pages/allpost.js
+++ prepare/front/pages/allpost.js
@@ -23,14 +23,14 @@ const AllPosts = () => {
     accusePostError,
     accuseMessage,
   } = useSelector((state) => state.post);
-  // useEffect(() => {
-  //   if (me) {
-  //     const lastId = 0;
-  //     dispatch({
-  //       type: LOAD_POSTS_REQUEST,
-  //     });
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (me) {
+      const lastId = 0;
+      dispatch({
+        type: LOAD_POSTS_REQUEST,
+      });
+    }
+  }, []);
 
   useEffect(() => {
     if (retweetError) {
@@ -86,22 +86,22 @@ const AllPosts = () => {
   );
 };
 
-export const getServerSideProps = wrapper.getServerSideProps(
-  async (context) => {
-    const cookie = context.req ? context.req.headers.cookie : '';
-    axios.defaults.headers.Cookie = '';
-    if (context.req && cookie) {
-      axios.defaults.headers.Cookie = cookie;
-    }
+// export const getServerSideProps = wrapper.getServerSideProps(
+//   async (context) => {
+//     const cookie = context.req ? context.req.headers.cookie : '';
+//     axios.defaults.headers.Cookie = '';
+//     if (context.req && cookie) {
+//       axios.defaults.headers.Cookie = cookie;
+//     }
 
-    context.store.dispatch({
-      type: LOAD_MY_INFO_REQUEST,
-    });
-    context.store.dispatch({
-      type: LOAD_POSTS_REQUEST,
-    });
-    context.store.dispatch(END);
-    await context.store.sagaTask.toPromise();
-  }
-);
+//     context.store.dispatch({
+//       type: LOAD_MY_INFO_REQUEST,
+//     });
+//     context.store.dispatch({
+//       type: LOAD_POSTS_REQUEST,
+//     });
+//     context.store.dispatch(END);
+//     await context.store.sagaTask.toPromise();
+//   }
+// );
 export default AllPosts;
