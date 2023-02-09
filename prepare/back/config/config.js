4c12a8408e271eba3127919bd7825fa2fe1c461b
hajung00 <gkwjd719@naver.com>
2023-02-09 16:29:40 +0900

    fix: database

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -14,7 +14,7 @@ module.exports = {
   test: {
     username: 'root',
     password: null,
-    database: 'database_test',
+    database: 'react-nodebird',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
